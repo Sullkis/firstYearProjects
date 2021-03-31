9786f6ad2e5196d896ab05c25f4d4fb19bda2fdf
Sullkis <78040027+Sullkis@users.noreply.github.com>
2021-03-31 16:36:49 +0100

    changed

--- webroot/mini/login.php
+++ webroot/mini/login.php
@@ -1,12 +1,40 @@
 <!DOCTYPE html>
 <html>
-    <body>
+    <head>
+        <meta charset="utf-8">
+        <title>Suleiman's Portfolio - Sign In</title>
+        <link rel="stylesheet" href="reset.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+<?php
 
-        <h1>my first </h1>
+include 'includes\dbh.php';
 
-            <?php
-            echo "hello";
-            ?>
+$uname = $_POST["uname"];
+$password = $_POST["password"];
 
-    </body>
+$sql = "SELECT * FROM USERS WHERE email='$uname' AND password='$password'";
+$result = mysqli_query($conn,$sql);
+
+$firstName = mysqli_query($conn,"SELECT firstName FROM USERS WHERE email='$uname'");
+$lastName = mysqli_query($conn,"SELECT lastName FROM USERS WHERE email='$uname'");
+$id = mysqli_query($conn,"SELECT ID FROM USERS WHERE email='$uname'");
+
+if(mysqli_num_rows($result) == 1){
+    while($row = $result->fetch_assoc()) {
+        session_start();
+        session_id($id);
+        $_SESSION['firstName'] = $row["firstName"];
+        $_SESSION['lastName'] = $row["lastName"];
+        $_SESSION['email'] = $uname;
+        $_SESSION['login'] = true;
+      }
+      header('Location: addPost.html');
+}
+else{
+    $fail = "Login credentials invalid!";
+    echo "<h3> $fail </h3><br>";
+}
+close($conn);
+?>
 </html>
