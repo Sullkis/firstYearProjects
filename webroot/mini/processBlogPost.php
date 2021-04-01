3b09efc1e185c6f7cf1043056b2bf5f7e83381c9
Sullkis <78040027+Sullkis@users.noreply.github.com>
2021-04-01 16:06:14 +0100

    changed p2

---
+++ webroot/mini/processBlogPost.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'connection.php';
+
+date_default_timezone_set('UTC');
+$postedTime = date("jS F Y, H:i")." UTC";
+
+$title = $_POST["title"];
+$postCont = $_POST["post-cont"];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $sql = "INSERT INTO BLOGPOSTS(datePosted, postTitle,postCont) 
+    VALUES ($postedTime, $title, $postCont)";
+
+    if ($conn->query($sql) === TRUE) {
+
+        header("Location: viewBlog.php");
+
+    }
+    else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+};
+
+?>
