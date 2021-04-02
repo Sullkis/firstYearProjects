560bf530bb0ec0a9103f536ad00dc34dafb7a8e9
Sullkis <78040027+Sullkis@users.noreply.github.com>
2021-04-02 20:41:50 +0100

    Update sortBlogEntries.php

--- webroot/mini/sortBlogEntries.php
+++ webroot/mini/sortBlogEntries.php
@@ -7,22 +7,22 @@ function sortarray($array,$datekey){
     }
     function sort_arr($array,$key){
         foreach ($array as $k => $v) {
-            $arrP1[] = strtolower($v[$key]);
+            $b[] = strtolower($v[$key]);
         }
     
-        arsort($arrP1);
+        arsort($b);
     
-        foreach ($arrP1 as $k => $v) {
-            $arrP2[] = $array[$k];
+        foreach ($b as $k => $v) {
+            $c[] = $array[$k];
         }
-        return $arrP2;
+        return $c;
     };
     $sorted = sort_arr($newArray,$datekey);
 
 
     foreach ($sorted as &$value) {
     for ($i=0; $i < count($array); $i++) { 
-        $value[$datekey] = date("jS F Y, H:i", $value[$datekey])." UTC";
+        $value[$datekey] = date("jS F Y, H:i", $value[$array])." UTC";
     }
 }
 return $sorted;
