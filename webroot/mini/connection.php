02847c293c0b9b08be2d80dffd28045aa33014e5
Sullkis <78040027+Sullkis@users.noreply.github.com>
2021-04-12 21:33:18 +0100

    Update connection.php

--- webroot/mini/connection.php
+++ webroot/mini/connection.php
@@ -1,20 +1,15 @@
 <?php
 
-// $dbServerName = "localhost";
-// $dbUsername = "root";
-// $dbPassword = "";
-// $dbName = "ecs417";
+    $dbhost = getenv("MYSQL_SERVICE_HOST"); 
+    $dbport = getenv("MYSQL_SERVICE_PORT"); 
+    $dbuser = getenv("DATABASE_USER"); 
+    $dbpwd = getenv("DATABASE_PASSWORD"); 
+    $dbname = getenv("DATABASE_NAME"); 
+// Creates connection 
+ $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname); 
+// Checks connection 
 
-$dbhost = getenv("MYSQL_SERVICE_HOST"); 
-$dbport = getenv("MYSQL_SERVICE_PORT"); 
-$dbuser = getenv("DATABASE_USER"); 
-$dbpwd = getenv("DATABASE_PASSWORD"); 
-$dbname = getenv("DATABASE_NAME"); 
-
-// Creates connection
-$conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
-
-// Checks connection
-if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);}
+if ($conn->connect_error) {     
+    die("Connection failed: " . $conn->connect_error); 
+ } 
 ?>
